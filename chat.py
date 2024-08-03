9f5e3441d2d7208478d62ecdf76d32a35c4b6bce
Keval Jamnapara <132595224+Keval1306@users.noreply.github.com>
2024-08-03 15:13:50 +0530

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+model = genai.GenerativeModel('gemini-1.0-pro-latest')
+
+st.title("Welcome our chatbot:")
+chat_avatar = "ai logo.jpg"
+user_avtar = "user logo.png"
+with st.chat_message("assistant", avatar=chat_avatar):
+    st.write("How can I assist you?")
+
+with st.chat_message("user",avatar=user_avtar):
+    user_message = st.chat_input("Enter your query:")
+response = ""
+if user_message:
+    response = model.generate_content(user_message)
+
+    if response and response.candidates:
+        response = response.candidates[0].content.parts[0].text
+
+if user_message:
+    st.write(response)
+
+
